9c844eea7e2c810308a6825274e2034fd21d3362
Shivaji Chalise <respond2shivaji@gmail.com>
2024-04-24 09:22:28 +0545

    updating with my info

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,186 @@
 import {
-  AmbitLogo,
   BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
   ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
   JarockiMeLogo,
   JojoMobileLogo,
   Minimal,
-  MobileVikingsLogo,
   MonitoLogo,
   NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
   YearProgressLogo,
 } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Shivaji Chalise",
+  initials: "SC",
+  location: "Pokhara, Nepal",
+  locationLink: "https://www.google.com/maps/place/Pokhara",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Full Stack Web Developer focused on being a strong problem solver and building products with extra attention to detail.",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "As a Full Stack Web Developer, I enjoy experimenting with new technologies to expand my expertise and thrive on challenges. Passionate about technology, I've explored various techstacks. Currently, I work mostly with Laravel, Tailwind CSS, TypeScript, React, Node.js, and MYSQL with over 2 years of experience in Laravel.",
+  avatarUrl:
+    "https://media.licdn.com/dms/image/D4E03AQGCXg2IkSGobQ/profile-displayphoto-shrink_400_400/0/1707408571802?e=1719446400&v=beta&t=3qngRIi6KCVSwgiPRoyAf4T9WnCZMdHZA0nql87bPmk",
+  personalWebsiteUrl: "https://shivajichalise.com.np",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "respond2shivaji@gmail.com",
+    tel: "+977-9840610177",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/shivajichalise",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/shivajichalise/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Gandaki College of Engineering and Science",
+      degree: "Bachelors in Software Engineering",
+      start: "2019",
+      end: "Present",
+    },
+    {
+      school: "Pokhara Engineering College",
+      degree: "Diploma in Computer Engineering",
+      start: "2017",
+      end: "2019",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "Zerone Technology",
+      link: "https://zeronetechnology.com.np/",
       badges: [],
-      title: "Android Developer → Lead Android Developer",
+      title: "Intern → Junior Full Stack Web Developer",
       logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      start: "2022",
+      end: "Present",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+        "Created dynamic web applications for diverse clients while transitioning into the role of project maintainer for existing projects. Continuously enhancing skills and delivering impactful solutions as a Junior Full Stack Web Developer since 2022.",
     },
     {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
+      company: "Zerone Technology",
+      link: "https://www.zeronetechnology.com.np",
       badges: [],
-      title: "C/C++ Developer",
+      title: "Intern",
       logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      start: "2019",
+      end: "2019",
+      description:
+        "Completed a 45-day internship, sub-merging myself in the world of software development, with a primary focus on Laravel. Thrived in a collaborative team environment, honing my skills and gaining valuable experience.",
     },
   ],
   skills: [
+    "PHP",
+    "Laravel",
+    "MYSQL",
     "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
+    "React",
     "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Express.js",
+    "MongoDB",
+    "GIT",
+    "Linux",
+    "Bash",
+    "Docker",
   ],
   projects: [
     {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
+      title: "Pokhara Marathon",
+      techStack: ["Laravel", "Bootstrap", "Blade"],
+      description:
+        "A web app for runners to register in race event with an intuitive admin dashboard to manage users, race registrations, certificates etc.",
       logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "pokharamarathon.org",
+        href: "https://app.pokharamarathon.org/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
+      title: "Rays Motors",
+      techStack: ["React", "Styled Components", "Laravel"],
+      description: "A web app for ISUZU vehicle showroom.",
+      logo: ConsultlyLogo,
+      link: {
+        label: "raysmotors.com.np",
+        href: "https://raysmotors.com.np/",
+      },
+    },
+    {
+      title: "Miraj Furniture",
+      techStack: ["React"],
+      description: "A website for Miraj Furnitures. static",
       logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "mirajfurniture.com.np",
+        href: "https://mirajfurniture.com.np/",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
+      title: "Shivaji Chalise",
+      techStack: ["Side Project", "React"],
+      description: "Personal website/blog for myself.",
       logo: JarockiMeLogo,
       link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
+        label: "shivajichalise.com.np",
+        href: "https://shivajichalise.com.np/",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Yakeen Kapali",
+      techStack: ["Side Project", "React", "Material UI"],
+      description: "Personal website for my friend; a graphic designer.",
+      logo: JarockiMeLogo,
+      link: {
+        label: "yakeenkapali.com.np",
+        href: "https://yakeenkapali.com.np/",
+      },
+    },
+    {
+      title: "NRB Forex",
+      techStack: ["Internal tool", "Custom Package", "Laravel"],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
+        "A laravel package to consume Nepal Rastra Bank's forex api.",
       logo: Minimal,
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "github.com",
+        href: "https://github.com/kodefarmers/nrb-forex",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Snippy Snapy Suu",
+      techStack: [
+        "Side Project",
+        "React",
+        "TypeScript",
+        "PostgreSQL",
+        "Node.js",
+        "Express.js",
+      ],
       description:
-        "Generates beautiful wallpapers using random shapes and gradients",
+        "A simple code snippet web app. It is currently in developement.",
       logo: BarepapersLogo,
       link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
+        label: "github.com",
+        href: "https://github.com/shivajichalise/snippysnapysuu",
       },
     },
     {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
+      title: "Event Mate",
+      techStack: ["College Project", "Laravel", "React"],
+      description: "An event management web app.",
       logo: YearProgressLogo,
       link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
         label: "github.com",
-        href: "https://parabol.co/",
+        href: "https://github.com/shivajichalise/eventmate",
       },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
